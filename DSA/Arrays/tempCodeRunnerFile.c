1666f284aa8f3ab4fb3aa38b0930c22a2ad18ff0
Himanshu Tanwar <himanshutanwar.official21@gmail.com>
2026-02-03 15:56:04 +0530

    completed array in compiler

--- DSA/Arrays/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-int A[5]; // this will create a array with 5 garbage values
-    // int A[5] = {1,2,3,4,5};
-    // int A[5] = {1,2}; // this will be A= {1,2,0,0,0}
-    // int A[5] = {0}; //this will create an array with all zero at 5 locations
-    // int A[]= {1,2,3,4,5};
