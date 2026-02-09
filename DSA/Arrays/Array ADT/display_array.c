01799c6ed2e006326cef285b0cbc8d65af57b4be
Himanshu Tanwar <himanshutanwar.official21@gmail.com>
2026-02-09 08:54:41 +0530

    initialising array

--- DSA/Arrays/Array ADT/display_array.c
+++ DSA/Arrays/Array ADT/display_array.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
-    int *A;
-    int n,i;
-    printf("Enter the size of array: ");
-    scanf("%d",&n);
-    A = (int *)malloc(n*sizeof(int));
-    printf("Enter the elements of array: ");
-    for(i=0;i<n;i++){
-        scanf("%d",&A[i]);
-    }
+    struct Array{
+        int *A;
+        int size;
+        int length;
+    };
+    struct Array arr;
+    printf("Enter size of an array:-\n");
+    scanf("%d\n",&arr.size);
+    arr.A =(int*)malloc(arr.size*sizeof(int));
+    arr.length = 0;
+    
 }
