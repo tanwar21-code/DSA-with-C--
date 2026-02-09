ea075cb8cbc3a3bb0915d74f70dd7fcf4bc3a2fa
Himanshu Tanwar <himanshutanwar.official21@gmail.com>
2026-02-09 09:03:54 +0530

    completed display function

--- DSA/Arrays/Array ADT/display_array.c
+++ DSA/Arrays/Array ADT/display_array.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-    struct Array{
+struct Array{
         int *A;
         int size;
         int length;
     };
+
+void Display(struct Array arr){
+    int i;
+    printf("Elements of array are:-");
+    for(i = 0;i<arr.length;i++){
+        printf("%d ",arr.A[i]);
+    }
+}
+
+int main(){
+    
     struct Array arr;
+    int n,i;
     printf("Enter size of an array:-\n");
-    scanf("%d\n",&arr.size);
+    scanf("%d",&arr.size);
     arr.A =(int*)malloc(arr.size*sizeof(int));
     arr.length = 0;
+    printf("Enter number of numbers in array:-\n");
+    scanf("%d",&n);
+    printf("Enter the elements of the array one by one:-\n");
+    for(i = 0;i<n;i++){
+        scanf("%d",&arr.A[i]);
+    }
+    arr.length = n;
+    Display(arr);
+
     
+
 }
